5c9ae4bed46b9def11f506aba1ad5b06cf1b0fba
Dimitri Lozeve <dimitri@lozeve.com>
2020-10-30 20:01:45 +0100

    Declare a Racket package

---
+++ info.rkt
@@ -0,0 +1,6 @@
+#lang info
+(define collection 'use-pkg-name)
+(define deps '("base"))
+(define build-deps '("racket-doc" "rackunit-lib"))
+(define pkg-desc "Tools for project management and planning")
+(define version "0.1")
---
+++ main.rkt
@@ -0,0 +1,7 @@
+#lang racket/base
+
+(provide (all-from-out "project.rkt")
+	 (all-from-out "gantt.rkt"))
+
+(require "project.rkt"
+	 "gantt.rkt")
