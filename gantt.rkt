7f5b569037aef385853eb3b05d9f15f9baa7443d
Dimitri Lozeve <dimitri@lozeve.com>
2020-10-30 19:25:01 +0100

    Add tests for Gantt charts generation

--- gantt.rkt
+++ gantt.rkt
@@ -1,30 +1,53 @@
 #lang racket/base
 
+(provide gantt)
+
 (require racket/stream
 	 "project.rkt")
 
-(provide gantt)
+(module+ test
+  (require rackunit))
 
 (define (task-max-work task)
   (apply max (hash-values (task-work task))))
 
+(module+ test
+  (define w (work 'bob 3 'alice 2))
+  (define t1 (task "" (inputs) (actions) (outcomes) (deliverables) w))
+  (define t2 (task "Task" (inputs) (actions) (outcomes) (deliverables) (work 'bob 4) (milestone 'm)))
+  (check-eq? (task-max-work t1) 3))
+
 (define (group-max-work group)
   (stream-fold + 0 (stream-map task-max-work group)))
+
+(module+ test
+  (define grp (group "Group" t1 t2))
+  (check-eq? (group-max-work grp) (+ 3 4)))
 
 (define (project-max-work project)
   (stream-fold + 0 (stream-map task-max-work (project-tasks-stream project))))
 
+(module+ test
+  (define prj (project "Project" grp))
+  (check-eq? (project-max-work prj) (group-max-work grp)))
+
 (define (task-start-times group group-start-time)
   (reverse (cdr (stream-fold
-		  (lambda (acc t) (cons (+ (car acc) (task-max-work t)) acc))
-		  (list group-start-time)
-		  group))))
+		 (lambda (acc t) (cons (+ (car acc) (task-max-work t)) acc))
+		 (list group-start-time)
+		 group))))
+
+(module+ test
+  (check-equal? (task-start-times grp 1) '(1 4)))
 
 (define (group-start-times project)
   (reverse (cdr (stream-fold
 		 (lambda (acc g) (cons (+ (car acc) (group-max-work g)) acc))
 		 '(1)
 		 project))))
+
+(module+ test
+  (check-equal? (group-start-times prj) '(1)))
 
 (define (gantt project)
   (printf "\\documentclass{standalone}~n")
