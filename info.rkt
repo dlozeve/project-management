b44f123bcebe873b4be3fd64b05417a804240a63
Dimitri Lozeve <dimitri@lozeve.com>
2020-10-30 20:21:32 +0100

    Add coverage tools

--- info.rkt
+++ info.rkt
@@ -1,6 +1,6 @@
 #lang info
 (define collection 'use-pkg-name)
 (define deps '("base"))
-(define build-deps '("racket-doc" "rackunit-lib"))
+(define build-deps '("racket-doc" "rackunit-lib" "cover-lib"))
 (define pkg-desc "Tools for project management and planning")
 (define version "0.1")
